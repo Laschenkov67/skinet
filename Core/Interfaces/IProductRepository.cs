f2507321ab46b56ea425073f1603c5fa827c946f
Artem <Artem@DESKTOP-JJ9O5GM>
2021-01-06 22:28:16 +0300

    Add repository and context part-2

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -7,6 +7,8 @@ namespace Core.Interfaces
     public interface IProductRepository
     {
         Task<Product> GetProductByIdAsync(int id);
+
+        //Представляем доступную только для чтения коллекцию элементов
         Task<IReadOnlyList<Product>> GetProductsAsync();
         Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
         Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
